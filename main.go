5aabdaba33c57b1a2ead5d6518096c364ebc790d
Mikaël Francoeur <mikael.francoeur@hotmail.com>
2023-01-11 22:09:40 -0500

    Various fixes.

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 )
 
 func main() {
-	metricsChan := make(chan float64)
+	metricsChan := make(chan float64, 1000)
 	exporter := metrics.NewExporter(metricsChan)
-	metricsServer := server.NewServer(metricsChan, exporter.LastPushed)
-	go exporter.Start()
+	metricsServer := server.NewServer(metricsChan, exporter.LastReceived)
+	go func() {
+		err := exporter.Start()
+		if err != nil {
+			panic(err)
+		}
+	}()
 
 	if err := http.ListenAndServe(":8080", http.HandlerFunc(metricsServer.Serve)); err != nil {
 		panic(err)
--- metrics/exporter.go
+++ metrics/exporter.go
@@ -2,7 +2,9 @@ package metrics
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
 	"log"
@@ -10,43 +12,51 @@ import (
 )
 
 type Exporter struct {
-	metricsChan <-chan float64
-	client      *cloudwatch.Client
-	lastPushed  *float64
+	metricsChan  <-chan float64
+	client       *cloudwatch.Client
+	lastReceived *float64
 }
 
 func NewExporter(metricsChan <-chan float64) *Exporter {
-	return &Exporter{metricsChan: metricsChan, lastPushed: aws.Float64(0)}
+	return &Exporter{metricsChan: metricsChan, lastReceived: aws.Float64(0)}
 }
 
-func (e Exporter) Start() {
-	e.client = cloudwatch.New(cloudwatch.Options{})
+func (e Exporter) Start() error {
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return fmt.Errorf("loading config: %v", err)
+	}
 
-	tick := time.Tick(1 * time.Minute)
+	e.client = cloudwatch.NewFromConfig(cfg)
+
+	tick := time.Tick(10 * time.Second)
 
 	for {
 		select {
 		case _ = <-tick:
 			var metrics []float64
-			for metric := range e.metricsChan {
-				*e.lastPushed = metric
+			for len(e.metricsChan) > 0 {
+				metric := <-e.metricsChan
 				metrics = append(metrics, metric)
+				*e.lastReceived = metric
 			}
-			e.pushMetrics(metrics)
+			if len(metrics) > 0 {
+				e.pushMetrics(metrics)
+			}
 		}
 	}
 }
 
-func (e Exporter) LastPushed() float64 {
-	if e.lastPushed == nil {
-		return 0
-	}
-	return *e.lastPushed
+func (e Exporter) LastReceived() float64 {
+	return *e.lastReceived
 }
 
 func (e Exporter) pushMetrics(metrics []float64) {
+	log.Printf("pushing metrics %v", metrics)
+
 	ctx, cancelFunc := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancelFunc()
+
 	_, err := e.client.PutMetricData(ctx,
 		&cloudwatch.PutMetricDataInput{
 			MetricData: []types.MetricDatum{{
@@ -55,9 +65,8 @@ func (e Exporter) pushMetrics(metrics []float64) {
 					Name:  aws.String("room"),
 					Value: aws.String("living room"),
 				}},
-				StorageResolution: aws.Int32(60),
-				Timestamp:         aws.Time(time.Now()),
-				Values:            metrics,
+				Timestamp: aws.Time(time.Now()),
+				Values:    metrics,
 			}},
 			Namespace: aws.String("Buildings"),
 		})
